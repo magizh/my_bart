b764dc16925dc253517571c1b90a38d040ce44ce
mahil <tmagizh@gmail.com>
2012-11-28 01:46:32 -0500

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,44 @@
+require 'net/http'
+require 'nokogiri'
+require "time"
+
+class HomeController < ApplicationController
+
+  def index
+   	start_time = Time.now 
+		start_time.rfc822 
+		pst = ActiveSupport::TimeZone["Pacific Time (US & Canada)"]
+		@pst= pst.at(start_time).strftime("%H:%M %p %Z")
+		noon = Time.parse("12:00:03 P.M. PST")
+		@is_noon = 1
+		# going to office 
+		# set your preferences here orig= ? and set API key 
+		if start_time < noon 
+			@is_noon = 0 
+			result = Net::HTTP.get(URI.parse('http://api.bart.gov/api/etd.aspx?cmd=etd&orig=dbrk&key=REPLACE_WITH_YOUR_KEY'))
+			doc = Nokogiri::XML(result)
+			@links = doc.xpath('//root/station/etd/estimate').map do |i|
+				# choose your direction
+				if i.xpath('direction').text()=="North"
+					{'destination' => i.parent().xpath('destination').text(),'dir'=> i.xpath('direction').text(),
+					'minutes'=> i.xpath('minutes').text(),'cars'=> i.xpath('length').text(),
+					'plat'=> i.xpath('platform').text(),'hex'=> i.xpath('hexcolor').text()}
+				end
+			end	
+		# going home 
+		# set your preferences here orig= ? and set API key 
+		else
+			result = Net::HTTP.get(URI.parse('http://api.bart.gov/api/etd.aspx?cmd=etd&orig=MONT&key=REPLACE_WITH_YOUR_KEY'))
+			doc = Nokogiri::XML(result)
+			@links = doc.xpath('//root/station/etd/estimate').map do |i|
+			  # choose your direction
+			 	if i.xpath('direction').text()=="South"
+			    {'destination' => i.parent().xpath('destination').text(),'dir'=> i.xpath('direction').text(),
+					'minutes'=> i.xpath('minutes').text(),'cars'=> i.xpath('length').text(),
+					'plat'=> i.xpath('platform').text(),'hex'=> i.xpath('hexcolor').text()}
+			 	end
+			end
+		end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bart::Application.config.secret_token = '960a007ed2e0cee3daefd7582dd66b1077d995d4fc48ca1e02f290b59902f379b04cda3ab2a36a96857766d864b4589dfee72b6a06bdefed1c26d57ae5087443'
